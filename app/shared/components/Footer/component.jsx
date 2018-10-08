9fee2095500e050e15ee083ea18e522072c4f668
Joel Mitchell <joel.mitchell@cxpartners.co.uk>
2018-10-08 15:59:00 +0100

    Tweaks to nav and placement

--- app/shared/components/Footer/component.jsx
+++ app/shared/components/Footer/component.jsx
@@ -1,15 +1,14 @@
 import React from 'react'
 import Nav from '../Nav/component.jsx'
 import { footer, footerUtility, footerButton } from '../../fixtures/navigation.js'
-import ReactGA from 'react-ga'
 
 const Footer = props => {
   return (
     <footer className='footer' role='contentinfo'>
       <section className='footer__inner'>
         <Nav id='footer-phone' className='displaced-top' navigation={footerButton}/>
-        <Nav className='navbar-expand-sm' id='navigation-footer' navigation={footer}/>
-        <Nav className='navbar-expand-sm navbar-muted' id='navigation-footer-utility' navigation={footerUtility}/>
+        <Nav className='navbar-expand navbar-raised' id='navigation-footer' navigation={footer}/>
+        <Nav className='navbar-expand navbar-muted' id='navigation-footer-utility' navigation={footerUtility}/>
       </section>
     </footer>
   )
--- app/shared/components/LinkItem/component.jsx
+++ app/shared/components/LinkItem/component.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 import Icon from '../Icon/component.jsx'
 
 const LinkItem = props => {
+  let label = props.icon ? <span className='btn__text'>{props.label}</span> : props.label
   return (
     <li className={props.className}>
-      <a className='nav-link' href={props.url} role='menuitem' onClick={(e) => props.clickHandler(props.tracking, e)}>{props.icon && <Icon {...props.icon}/>}{props.label}</a>
+      <a className='nav-link' href={props.url} role='menuitem' onClick={(e) => props.clickHandler(props.tracking, e)}>{props.icon && <Icon {...props.icon}/>}{label}</a>
     </li>
   )
 }
