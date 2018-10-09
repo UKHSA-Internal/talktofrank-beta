538a1bf5401937485aadf75a2067c5c6cb399051
Joel Mitchell <joel.mitchell@cxpartners.co.uk>
2018-10-09 07:58:08 +0100

    Added divider and styles - updated navbar-nav

--- app/shared/components/Footer/component.jsx
+++ app/shared/components/Footer/component.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Nav from '../Nav/component.jsx'
+import Divider from '../Divider/component.jsx'
 import { footer, footerUtility, footerButton } from '../../fixtures/navigation.js'
 
 const Footer = props => {
@@ -8,6 +9,7 @@ const Footer = props => {
       <section className='footer__inner'>
         <Nav id='footer-phone' className='displaced-top' navigation={footerButton}/>
         <Nav className='navbar-expand navbar-raised' id='navigation-footer' navigation={footer}/>
+        <Divider className='hr--inverse'/>
         <Nav className='navbar-expand navbar-muted' id='navigation-footer-utility' navigation={footerUtility}/>
       </section>
     </footer>
