b528d78ebeb33a852fcdabdf6af9d0d9a33f9b5a
Joel Mitchell <joel.mitchell@cxpartners.co.uk>
2018-10-17 10:06:20 +0100

    Popping it in gridcol - minor patch up

--- app/shared/components/PageGeneral/component.jsx
+++ app/shared/components/PageGeneral/component.jsx
@@ -3,6 +3,7 @@ import Masthead from '../Masthead/component.jsx'
 import Footer from '../Footer/component.jsx'
 import GA from '../GoogleAnalytics/component.jsx'
 import Grid from '../Grid/component.jsx'
+import GridCol from '../GridCol/component.jsx'
 import Main from '../Main/component.jsx'
 import Accent from '../Accent/component.jsx'
 import { factory } from '../../factory.jsx'
@@ -12,10 +13,12 @@ export default class PageGeneral extends React.PureComponent {
     return (
       <React.Fragment>
         <Masthead path={this.props.location}/>
-        <Main className='main--full-width'>
+        <Main>
           <Accent>
             <Grid>
+              <GridCol className='col-12 col-md-8'>
               { factory(this.props) }
+              </GridCol>
             </Grid>
           </Accent>
         </Main>
