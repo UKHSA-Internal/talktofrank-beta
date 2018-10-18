0b48c8ab8bff43ea87dc811fcfca9565ab9edd68
Joel Mitchell <joel.mitchell@cxpartners.co.uk>
2018-10-18 11:30:04 +0100

    Fix: TTF-499, TTF-511 - hero
    https://digitaltools.phe.org.uk/browse/TTF-499
    https://digitaltools.phe.org.uk/browse/TTF-511

--- app/shared/components/SplitText/component.jsx
+++ app/shared/components/SplitText/component.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 
-const SplitText = ({text, wrapper, modifiers = 'heading-inverted-test', highlight}) => {
-  let str = text.split(' ')
+const SplitText = ({text, wrapper, modifiers = 'heading-inverted--inline', highlight}) => {
+  // let str = text.split(' ')
+  let str = text // for the moment - just do this
   let Wrapper = `${wrapper || 'h2'}`
-  str = str.map((item, i) => {
-    let high = highlight && highlight.indexOf(i) !== -1 ? 'highlighted' : null
-    return <span key={i} className={high}>{item} </span>
-  })
-  return <Wrapper className={modifiers}>{str}</Wrapper>
+  // str = str.map((item, i) => {
+  //   let high = highlight && highlight.indexOf(i) !== -1 ? 'highlighted' : null
+  //   return <span key={i} className={high}>{item} </span>
+  // })
+  return <Wrapper className={modifiers}><span dangerouslySetInnerHTML={{__html: str}} /></Wrapper>
 }
 export default SplitText
