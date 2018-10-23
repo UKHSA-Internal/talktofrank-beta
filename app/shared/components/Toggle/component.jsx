3ba99b2aba8ebd00c00aa3653809222a0e5b73c9
Joel Mitchell <joel.mitchell@cxpartners.co.uk>
2018-10-23 17:09:28 +0100

    Fix: TTF-613 - make whole section a link
    https://digitaltools.phe.org.uk/browse/TTF-613

--- app/shared/components/Toggle/component.jsx
+++ app/shared/components/Toggle/component.jsx
@@ -54,7 +54,7 @@ export default class Toggle extends React.PureComponent {
         <div className='constrain-narrow'>
           <Grid>
             <GridCol className='col-12 col-md-7 offset-md-3 bordered'>
-              <h3 className='h4 sm-spacing--tight'>
+              <h3 className='h4'>
                 <a role='button' href={`#${id}`} data-target={`#${id}`} className={toggleClass} onClick={this.toggle.bind(this)} aria-expanded={this.state.visible} aria-controls={`section-${id}`}>
                 {text}
                 </a>
