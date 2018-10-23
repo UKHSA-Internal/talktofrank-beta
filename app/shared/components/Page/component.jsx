2aa30a669c3550438688c26006a8349f7036710a
Joel Mitchell <joel.mitchell@cxpartners.co.uk>
2018-10-23 17:00:25 +0100

    Fix: TTF-610
    https://digitaltools.phe.org.uk/browse/TTF-610

--- app/shared/components/Page/component.jsx
+++ app/shared/components/Page/component.jsx
@@ -73,14 +73,14 @@ const Page = props => {
             {props.fields.durationDetail && <Longform text={props.fields.durationDetail} />}
             {props.fields.durationMethodOfTaking && props.fields.durationMethodOfTaking.map((v, i) => {
               return (
-                <article className='panel panel--padding-small panel--has-heading' key={i}>
+                <aside className='panel panel--padding-small panel--has-heading' key={i}>
                   <Heading type='h3' text={v.fields.methodName} className='h4 inverted displaced-top'/>
                   <dl className='definition-list'>
                     <dt>Start to feel effects:</dt><dd dangerouslySetInnerHTML={{__html: v.fields.methodEffectsStart}} />
                     <dt>The effects last for:</dt><dd dangerouslySetInnerHTML={{__html: v.fields.methodEffectsDuration}} />
                     {v.fields.methodAfterEffects && <React.Fragment><dt>After effects:</dt><dd dangerouslySetInnerHTML={{__html: v.fields.methodAfterEffects}}/></React.Fragment>}
                   </dl>
-                </article>
+                </aside>
               )
             })}
             {props.fields.durationDetectable && <React.Fragment><Heading {...modifiers} text='How long will it be detectable?'/><Longform text={props.fields.durationDetectable} /></React.Fragment>
