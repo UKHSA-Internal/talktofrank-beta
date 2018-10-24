d207cf9b3cee4ed777685db461019d8a8672a692
Joel Mitchell <joel.mitchell@cxpartners.co.uk>
2018-10-24 10:35:49 +0100

    Fix: TTF-524 - 404 page footer
    https://digitaltools.phe.org.uk/browse/TTF-524

--- app/shared/components/PageNotFound/component.jsx
+++ app/shared/components/PageNotFound/component.jsx
@@ -7,6 +7,7 @@ import Masthead from '../Masthead/component'
 import Main from '../Main/component'
 import Grid from '../Grid/component'
 import GridCol from '../GridCol/component'
+import Accent from '../Accent/component'
 
 const PageNotFound = () => {
   const text = '<p>If you entered a web address please check it was correct.</p>' +
@@ -17,12 +18,14 @@ const PageNotFound = () => {
     <React.Fragment>
       <Masthead path={'no-match'}/>
       <Main>
-        <Grid>
-          <GridCol className='col-12 col-md-8'>
-            <Heading type='h2' text='Page not found' />
-            <Longform text={text} />
-          </GridCol>
-        </Grid>
+        <Accent>
+          <Grid>
+            <GridCol className='col-12 col-md-8'>
+              <Heading type='h2' text='Page not found' />
+              <Longform text={text} />
+            </GridCol>
+          </Grid>
+        </Accent>
       </Main>
       <Footer/>
       <GA/>
