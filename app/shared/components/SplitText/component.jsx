be3bb83603114f1eeb431551b4b6e5880203c865
Joel Mitchell <joel.mitchell@cxpartners.co.uk>
2018-10-25 17:06:05 +0100

    Linting

--- app/shared/components/SplitText/component.jsx
+++ app/shared/components/SplitText/component.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const SplitText = ({text, wrapper, modifiers = 'heading-inline heading-inverted--inline'/*, highlight */}) => {
+const SplitText = ({text, wrapper, modifiers = 'heading-inline heading-inverted--inline'}) => {
   // this has been reworked - grab the old one from Github history
   let Wrapper = `${wrapper || 'h2'}`
   return <Wrapper className={modifiers}><span dangerouslySetInnerHTML={{__html: text}} /></Wrapper>
