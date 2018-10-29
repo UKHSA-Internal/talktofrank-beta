cce4476beb32619348a419b9a650704b7ed52dc9
Joel Mitchell <joel.mitchell@cxpartners.co.uk>
2018-10-29 12:08:23 +0000

    Adding homepage feature block

--- app/shared/components/PageHome/component.jsx
+++ app/shared/components/PageHome/component.jsx
@@ -4,11 +4,11 @@ import Main from '../Main/component.jsx'
 import Heading from '../Heading/component.jsx'
 import Footer from '../Footer/component.jsx'
 import GA from '../GoogleAnalytics/component.jsx'
-import Button from '../Button/component.jsx'
 import Hero from '../Hero/component.jsx'
 import CardDeck from '../CardDeck/component.jsx'
 import Article from '../Article/component.jsx'
-import Accent from '../Accent/component.jsx'
+import Grid from '../Grid/component.jsx'
+import GridCol from '../GridCol/component.jsx'
 
 export default class PageHome extends React.PureComponent {
   render () {
@@ -106,10 +106,14 @@ export default class PageHome extends React.PureComponent {
       <React.Fragment>
         <Masthead path={this.props.location}/>
         <Hero {...hero}/>
-        <Accent className='list-offset'>
-          <Article {...featuredItem}/>
-        </Accent>
-        <CardDeck {...news} className='spacing-top--tight'/>
+        <Main>
+          <Grid>
+            <GridCol className='col-12 col-sm-10 offset-sm-1 list-offset'>
+              <Article {...featuredItem}/>
+            </GridCol>
+          </Grid>
+          <CardDeck {...news} className='spacing-top--tight'/>
+        </Main>
         <Footer />
         <GA/>
       </React.Fragment>
