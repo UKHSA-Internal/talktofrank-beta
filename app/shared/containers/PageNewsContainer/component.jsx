bad2d0947811a0686cd6837cb782dfea766f2a45
Andy Brace <contact@openbook.uk.com>
2018-11-13 13:25:51 +0000

    remvoing debug

--- app/shared/containers/PageNewsContainer/component.jsx
+++ app/shared/containers/PageNewsContainer/component.jsx
@@ -3,7 +3,6 @@ import { connect } from 'react-redux'
 import PageNews from '../../components/PageNews/component.jsx'
 
 const mapStateToProps = (state, ownProps) => {
-  console.log(state.app.pageData)
   return state.app.pageData
 }
 
