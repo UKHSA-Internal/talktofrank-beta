143b9eb97bd1c1805d76a3d366f6b420a34e0efd
Andy Brace <contact@openbook.uk.com>
2018-11-20 09:52:53 +0000

    adding webhook calls for publish/unpublish workflow

--- app/server/contentful/webhooks.js
+++ app/server/contentful/webhooks.js
@@ -1,10 +1,28 @@
 import { config } from 'config'
-
-const debug = require(`debug`)(`contentful-webhook`)
+import { removeMarkdown } from '../../shared/utilities'
+import {
+  formatGeneralContentEntryForSearch,
+  formatDrugEntryForSearch,
+  formatDrugNameEntryForSearch,
+  formatNewsEntryForSearch,
+  buildBulkDeleteAction,
+  buildBulkUpdateAction
+} from '../../shared/elasticSearch'
 const express = require('express')
 const bodyParser = require('body-parser')
 const router = express.Router()
 const Sentry = require('@sentry/node')
+const bodybuilder = require('bodybuilder')
+const resolveResponse = require('contentful-resolve-response')
+const contentful = require('contentful')
+const contentfulClient = contentful.createClient({
+  // This is the space ID. A space is like a project folder in Contentful terms
+  space: config.contentful.contentSpace,
+  environment: config.contentful.environment,
+  // This is the access token for this space. Normally you get both ID and the token in the Contentful web app
+  accessToken: config.contentful.contentAccessToken,
+  host: config.contentful.contentHost
+})
 
 /**
  * Add middleware to parse json
@@ -14,23 +32,187 @@ router.use(bodyParser.json({type: 'application/*'}))
 /**
  * Get page data
  */
-router.use('/', (req, res, next) => {
+router.use('/', async (req, res, next) => {
   try {
     const search = res.search
-    debug('\nRequests header\n', req.headers)
-    debug('\nRequests body\n', req.body)
-
     const webhookName = req.headers['x-contentful-topic']
-    debug(`Webhook called ${webhookName}`)
-
+    console.log(`\nWebhook called ${webhookName}`)
     switch (webhookName) {
       case 'ContentManagement.Entry.publish' :
-        search.indexer.indexSingleEntry(req.body)
+
+        if (req.body.sys.contentType.sys.id === config.contentful.contentTypes.drug) {
+          console.log(
+            `\nPublishing ${req.body.sys.contentType.sys.id}` +
+            ` entry id: ${req.body.sys.id}`
+          )
+
+          // Contentful webhook doesn't contain full entry payload so reloading
+          // using lib here
+          const contentfulResponse = await contentfulClient.getEntries({
+            'sys.id': req.body.sys.id,
+            include: 10
+          })
+
+          if (contentfulResponse.total === 0) {
+            console.log(`Error updating sys id ${req.body.sys.id}, not found in contentful`)
+            return
+          }
+
+          const entry = resolveResponse(contentfulResponse)[0]
+
+          // Remove any entries in the 'drug name' index that no longer exist
+          // in the list of drug synonyms
+          const query = bodybuilder()
+            .orFilter('term', 'entryId', entry.sys.id)
+            .build()
+
+          const drugNameResults = await search.search({
+            index: config.elasticsearch.indices.drugNames,
+            body: query
+          })
+          let itemsToDeleteFromSearch = []
+          drugNameResults.hits.hits
+            .filter(drugItem => drugItem._source.realName !== drugItem._source.name)
+            .map(drugItem => {
+              const existing = entry.fields.synonyms.findIndex(item => drugItem._source.name.toLowerCase() === item.toLowerCase())
+              if (existing === -1) {
+                itemsToDeleteFromSearch.push({id: drugItem._id, name: drugItem._source.name})
+              }
+            })
+
+          if (itemsToDeleteFromSearch.length > 0) {
+            buildBulkDeleteAction(
+              config.elasticsearch.indices.drugNames,
+              '_doc',
+              itemsToDeleteFromSearch,
+              search,
+              'name'
+            )
+          }
+
+          // Create new entries in the 'drug name' & 'drug text' indices
+          let formattedDrugsText = []
+          let formattedDrugsNames = []
+          const name = entry.fields.hasOwnProperty('drugName') ? entry.fields.drugName : null
+          formattedDrugsText.push(formatDrugEntryForSearch(name, entry))
+          formattedDrugsNames.push(formatDrugNameEntryForSearch(name, name, entry))
+
+          if (entry.fields.synonyms) {
+            entry.fields.synonyms
+              .map(synonymName => {
+                formattedDrugsNames.push(formatDrugNameEntryForSearch(synonymName, name, entry))
+              })
+          }
+
+          // Create bulk actions for ES
+          await buildBulkUpdateAction(
+            config.elasticsearch.indices.drug,
+            '_doc',
+            formattedDrugsText,
+            search,
+            'drugName'
+          )
+
+          await buildBulkUpdateAction(
+            config.elasticsearch.indices.drugNames,
+            '_doc',
+            formattedDrugsNames,
+            search,
+            'name'
+          )
+        }
+
+        // News && General content
+        if (req.body.sys.contentType.sys.id === config.contentful.contentTypes.page ||
+          req.body.sys.contentType.sys.id === config.contentful.contentTypes.news) {
+          console.log(
+            `Publishing ${req.body.sys.contentType.sys.id}` +
+            `entry id: ${req.body.sys.id}`
+          )
+          // exclude 'includeInSearch = false' values
+          // Contentful webhook doesn't contain full entry payload so reloading
+          // using lib here
+          const contentfulResponse = await contentfulClient.getEntries({
+            'sys.id': req.body.sys.id,
+            include: 10
+          })
+
+          if (contentfulResponse.total === 0) {
+            console.log(`Error updating sys id ${req.body.sys.id}, not found in contentful`)
+            return
+          }
+
+          let formattedContent = []
+          const entry = resolveResponse(contentfulResponse)[0]
+
+          if (entry.fields.includeInSearch && entry.fields.includeInSearch === false) {
+            console.log(`Skipping update of content sys id ${req.body.sys.id}, includeInSearch is false`)
+          }
+
+          const item = req.body.sys.contentType.sys.id === config.contentful.contentTypes.news
+            ? formatNewsEntryForSearch(entry) : formatGeneralContentEntryForSearch(entry)
+          formattedContent.push(item)
+
+          await buildBulkUpdateAction(
+            config.elasticsearch.indices.content,
+            '_doc',
+            formattedContent,
+            search,
+            'title'
+          )
+        }
         break
 
       case 'ContentManagement.Entry.unpublish' :
       case 'ContentManagement.Entry.delete' :
-        search.indexer.deleteSingleEntry(req.body)
+
+        if (req.body.sys.contentType.sys.id === config.contentful.contentTypes.drug) {
+          console.log(
+            `Publishing ${req.body.sys.contentType.sys.id}` +
+            ` entry id: ${req.body.sys.id}`
+          )
+
+          // Find existing entries in the 'drug name' index & remove
+          const query = bodybuilder()
+            .orFilter('term', 'entryId', req.body.sys.id)
+            .build()
+
+          const drugNameResults = await search.search({
+            index: config.elasticsearch.indices.drugNames,
+            body: query
+          })
+          let itemsToDeleteFromSearch = []
+          drugNameResults.hits.hits
+            .map(item => itemsToDeleteFromSearch.push({id: item._id, name: item._source.name}))
+
+          if (itemsToDeleteFromSearch.length > 0) {
+            buildBulkDeleteAction(
+              config.elasticsearch.indices.drugNames,
+              '_doc',
+              itemsToDeleteFromSearch,
+              search,
+              'name'
+            )
+          }
+
+          buildBulkDeleteAction(
+            config.elasticsearch.indices.drug,
+            '_doc',
+            [{id: req.body.sys.id}],
+            search
+          )
+        }
+
+        // News && General content
+        if (req.body.sys.contentType.sys.id === config.contentful.contentTypes.page ||
+        req.body.sys.contentType.sys.id === config.contentful.contentTypes.news) {
+          buildBulkDeleteAction(
+            config.elasticsearch.indices.content,
+            '_doc',
+            [{id: req.body.sys.id}],
+            search
+          )
+        }
         break
     }
 
