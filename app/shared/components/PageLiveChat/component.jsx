a648093c3807edfe21e716ecaafdf86fdb260463
Jon <jon@kode.uk.com>
2018-11-20 18:47:55 +0000

    Adding live chat

---
+++ app/shared/components/PageLiveChat/component.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import Accent from '../Accent/component'
+import Masthead from '../Masthead/component'
+import Divider from '../Divider/component'
+import Heading from '../Heading/component'
+import Footer from '../Footer/component'
+import GA from '../GoogleAnalytics/component'
+import Grid from '../Grid/component'
+import GridCol from '../GridCol/component'
+import Main from '../Main/component'
+import Longform from '../Longform/component'
+
+const PageLiveChat = props => {
+  return (
+    <React.Fragment>
+      <Masthead path={props.location}/>
+      <Accent className='accent--shallow'>
+        <Heading type='h1' className='h2 spacing-left spacing--single' text="Live Chat" />
+      </Accent>
+      <Divider className='hr--muted' />
+      <Main>
+        <Grid>
+          <GridCol className='col-md-8 offset-md-2'>
+            <iframe title="FRANK - livechat" frameborder="0" width="100%" height="450" src="https://smokefree.serco.com/visitor/EntryPageClosed.htm">FRANK - livechat</iframe>
+          </GridCol>
+        </Grid>
+      </Main>
+      <Footer />
+      <GA/>
+    </React.Fragment>
+  )
+}
+
+export default PageLiveChat
--- app/shared/newRoutes.jsx
+++ app/shared/newRoutes.jsx
@@ -39,6 +39,7 @@ const asyncGetHelpPageContainer = loadable(() => import(/*webpackChunkName: 'get
 const asyncPageSupportListContainer = loadable(() => import(/*webpackChunkName: 'support-centre-list'*/'./containers/PageSupportListContainer/component.jsx'))
 const asyncPageSupportContainer = loadable(() => import(/*webpackChunkName: 'support-centre'*/'./containers/PageSupportContainer/component.jsx'))
 const asyncPageContactSuccessContainer = loadable(() => import(/*webpackChunkName: 'support-centre'*/'./containers/PageContactSuccessContainer/component.jsx'))
+const asyncPageLiveChat = loadable(() => import(/*webpackChunkName: 'live-chat'*/'./components/PageLiveChat/component.jsx'))
 /* eslint-enable */
 
 export default [{
@@ -62,6 +63,11 @@ export default [{
       component: asyncPageDrug,
       loadData: ({params}) => fetchPage(params.drugName, 'drugs')
     },
+    {
+      path: '/livechat',
+      exact: true,
+      component: asyncPageLiveChat
+    },    
     {
       path: '/contact',
       exact: true,
