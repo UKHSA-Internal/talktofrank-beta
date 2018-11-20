e7de80c331c113a8f826990febc380f2aa7eacf6
Joel Mitchell <joel.mitchell@cxpartners.co.uk>
2018-11-20 09:02:33 +0000

    Fix: TTF-815 - pagination aria roles
    https://digitaltools.phe.org.uk/browse/https://digitaltools.phe.org.uk/browse/TTF-815

--- app/shared/components/Footer/component.jsx
+++ app/shared/components/Footer/component.jsx
@@ -7,7 +7,7 @@ const Footer = props => {
   return (
     <footer className='footer'>
       <section className='footer__inner'>
-        <Nav className='displaced-top' navigation={footerButton} visible='true'/>
+        <Nav role={null} className='displaced-top' navigation={footerButton} visible='true'/>
         <Nav role={null} className='navbar-expand navbar-raised' navigation={footer} visible='true'/>
         <Divider className='hr--inverse'/>
         <Nav role={null} className='navbar-expand navbar-muted' navigation={footerUtility} visible='true'/>
