0a330cb9fa6022bd9b3094edcb215dacfc1c37f3
Jon <jon@kode.uk.com>
2018-11-26 16:03:04 +0000

    Add maxlength to input

--- app/shared/components/FormGroup/component.jsx
+++ app/shared/components/FormGroup/component.jsx
@@ -16,13 +16,17 @@ const FormGroup = props => {
       <label htmlFor={id} className='form-label'>{props.label}</label>
       {props.hint && <FormHint id={props.hintId || null}>{props.hint}</FormHint>}
       {props.error && <ErrorMessage message={props.error} />}
-      <input className={inputClassNames} id={id} name={props.name} value={props.value} type='text' {...error} onChange={props.onChange} aria-describedby={props.hintId || null}/>
+      <input className={inputClassNames} maxLength={props.maxLength} id={id} name={props.name} value={props.value} type='text' {...error} onChange={props.onChange} aria-describedby={props.hintId || null}/>
     </div>
   )
 }
 
 FormGroup.propTypes = {
   id: PropTypes.string
+}
+
+FormGroup.defaultProps = {
+  maxLength: 300
 }
 
 export default FormGroup
