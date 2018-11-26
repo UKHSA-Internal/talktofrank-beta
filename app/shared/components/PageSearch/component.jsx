1ad6e9d21c17f8f9678058ab07c8d762d369ad20
Joel Mitchell <joel.mitchell@cxpartners.co.uk>
2018-11-26 11:20:07 +0000

    Fix: TTF-1025 - change emails
    https://digitaltools.phe.org.uk/browse/TTF-1025

--- app/shared/components/PageSearch/component.jsx
+++ app/shared/components/PageSearch/component.jsx
@@ -74,7 +74,7 @@ export default class SearchPage extends React.Component {
             <Heading type='h3' className='h5' text='Get in touch'/>
             <p><Anchor className='link-text' href='tel:03001236600' label='Call FRANK on 0300 123 6600'>Call: 0300 123 6600</Anchor></p>
             <p><Anchor className='link-text' href='sms:82111' label='Text FRANK on 82111'>Text: 82111</Anchor></p>
-            <p><Anchor className='link-text' href='mailto:03001236600' label='Send an email to FRANK at frank@talktofrank.com'>frank@talktofrank.com</Anchor></p>
+            <p><Anchor className='link-text' href='/contact' label='Send an email to FRANK'>Send an email</Anchor></p>
           </GridCol>
         </Grid>
 
