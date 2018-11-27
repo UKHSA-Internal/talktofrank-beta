1604b21fc3b534b3df3037105882efcf77e08615
Joel Mitchell <joel.mitchell@cxpartners.co.uk>
2018-11-27 14:28:46 +0000

    Fix: TTF-980 - pagination hover
    https://digitaltools.phe.org.uk/browse/TTF-980

--- app/shared/components/Head/component.jsx
+++ app/shared/components/Head/component.jsx
@@ -35,13 +35,7 @@ export default class Head extends React.Component {
         window.addEventListener('touchstart', function onFirstTouch() {
           document.documentElement.classList.remove('has-hover')
           window.removeEventListener('touchstart', onFirstTouch, false)
-        }, false)
-
-        document.getElementById('skip').addEventListener('click', function(e) {
-          e.preventDefault()
-          var target = document.getElementById('main')
-          target.focus()
-        })`
+        }, false)`
         }} />
       </head>
     )
