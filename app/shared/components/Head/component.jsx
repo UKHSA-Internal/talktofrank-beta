c6500a14908ada4301c34c1bdaf829dc432577fb
Andy Brace <contact@openbook.uk.com>
2018-11-28 10:34:38 +0000

    adding link to streetname in search + theme colour

--- app/shared/components/Head/component.jsx
+++ app/shared/components/Head/component.jsx
@@ -68,6 +68,7 @@ export default class Head extends React.Component {
         <meta content='on' httpEquiv='cleartype' />
         <meta name='format-detection' content='telephone=no' />
         <meta httpEquiv='X-UA-Compatible' content='IE=edge' />
+        <meta name='theme-color' content='#FFFFFF' />
         <link rel='stylesheet' href='/ui/css/main.css' />
         <script dangerouslySetInnerHTML={{__html:
         `
