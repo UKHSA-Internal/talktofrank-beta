da0585e9028ed254f2237f426138e1d73671f851
Joel Mitchell <joel.mitchell@cxpartners.co.uk>
2018-11-30 17:17:10 +0000

    Fix: TTF-1162 - homepage search form icon
    https://digitaltools.phe.org.uk/browse/TTF-1162

--- app/shared/utilities.js
+++ app/shared/utilities.js
@@ -83,7 +83,7 @@ export function exists (obj, key) {
   })
 }
 
-// not great - needs a bit more flexibilty
+// not great - needs a bit more flexibility
 // it mirrors the cms but would be nice to
 // have the freedom to add more breakoints
 export function imageMap (obj) {
