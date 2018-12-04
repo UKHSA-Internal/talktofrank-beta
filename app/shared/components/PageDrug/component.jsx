fd4c7c8333c1276a2cab6cab36354dcc454aa8a2
Joel Mitchell <joel.mitchell@cxpartners.co.uk>
2018-12-04 09:51:06 +0000

    Fix: TTF-1203 margin top on heading with image
    https://digitaltools.phe.org.uk/browse/TTF-1203

--- app/shared/components/PageDrug/component.jsx
+++ app/shared/components/PageDrug/component.jsx
@@ -34,8 +34,8 @@ const Page = props => {
             {props.fields.image && <GridCol className='col-12 col-md-3'>
               <Picture {...imageMap(props.fields.image)} />
             </GridCol>}
-            <GridCol className={'col-12 col-md-7 ' + (!props.fields.image ? 'offset-md-3' : null)}>
-              <Heading type='h1' text={props.fields.drugName} className='h2 inverted spacing-bottom--single'/>
+            <GridCol className={'col-12 col-md-7 ' + (!props.fields.image ? 'offset-md-3' : '')}>
+              <Heading type='h1' text={props.fields.drugName} className={`h2 inverted spacing-bottom--single ${props.fields.image ? 'has-image' : ''}`}/>
               {props.fields.synonyms && props.fields.synonyms[0] !== '' && <React.Fragment><p className='lead bold'>Also called:</p>
               <ul className='list-unstyled spacing-bottom--tight'>{props.fields.synonyms && props.fields.synonyms.map((item, i) => <li className={'list-inline-item inverted bold' + (syn !== item ? ' inverted--quiet' : '')} key={i}>{item}</li>)}</ul></React.Fragment>}
               <Longform text={props.fields.description} className='spacing-bottom--single'/>
