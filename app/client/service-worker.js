f2c7a4d61c811240b71152eee2b8f54cbab276db
Andy Brace <git-hub@openbook.uk.com>
2018-12-04 16:02:40 +0000

    attempting to use different offline policy

--- app/client/service-worker.js
+++ app/client/service-worker.js
@@ -33,11 +33,18 @@ if (workbox) {
     workbox.strategies.networkFirst()
   )
 
-  workbox.routing.registerRoute(
-    ({event}) => event.request.mode === 'navigate' && event.request.redirect === 'manual',
-    ({url}) => fetch(url.href, {credentials: 'same-origin', redirect: 'follow'})
-      .catch(() => {
-        return caches.match('/offline')
-      })
-  )
+  
+  const networkOnly = workbox.strategies.networkOnly()
+  const route = new workbox.routing.NavigationRoute(({event}) => {
+    return networkOnly.handle({event}).catch(() => caches.match('/offline'))
+  })
+  workbox.routing.registerRoute(route)
+  
+//   workbox.routing.registerRoute(
+//     ({event}) => event.request.mode === 'navigate' && event.request.redirect === 'manual',
+//     ({url}) => fetch(url.href, {credentials: 'same-origin', redirect: 'follow'})
+//       .catch(() => {
+//         return caches.match('/offline')
+//       })
+//   )
 }
