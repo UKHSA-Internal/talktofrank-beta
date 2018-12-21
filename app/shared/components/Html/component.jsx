f0967899d6352d5f27d152ce4676a31468ba556b
Andy Brace <contact@openbook.uk.com>
2018-12-21 13:51:13 +0000

    adding updating header for schema tags

--- app/shared/components/Html/component.jsx
+++ app/shared/components/Html/component.jsx
@@ -5,34 +5,40 @@ import Scripts from '../Scripts/component.jsx'
 import Head from '../Head/component.jsx'
 
 const Html = (props) => {
-  const schemaTags = {
-    '@context': 'http://schema.org',
-    '@type': 'GovernmentOrganization',
-    'name': 'FRANK',
-    'alternateName': 'Talk to Frank',
-    'url': 'https://www.talktofrank.com',
-    'logo': 'https://www.talktofrank.com/ui/svg/logo-frank--alt.svg',
-    'email': 'frank@talktofrank.com',
-    'contactPoint': [
-      {
-        '@type': 'ContactPoint',
-        'telephone': '+44300 123 6600',
-        'contactType': 'Emergency'
-      },
-      {
-        '@type': 'ContactPoint',
-        'telephone': '+44300 123 1099',
-        'contactType': 'Emergency',
-        'contactOption': ['HearingImpairedSupported']
-      }
-    ]
+  const { location } = props
+  let showSchema = false
+  let schemaTags
+  if (location && location.pathname === '/') {
+    showSchema = true
+    schemaTags = {
+      '@context': 'http://schema.org',
+      '@type': 'GovernmentOrganization',
+      'name': 'FRANK',
+      'alternateName': 'Talk to Frank',
+      'url': 'https://www.talktofrank.com',
+      'logo': 'https://www.talktofrank.com/ui/svg/logo-frank--alt.svg',
+      'email': 'frank@talktofrank.com',
+      'contactPoint': [
+        {
+          '@type': 'ContactPoint',
+          'telephone': '+44300 123 6600',
+          'contactType': 'Customer Service'
+        },
+        {
+          '@type': 'ContactPoint',
+          'telephone': '+44300 123 1099',
+          'contactType': 'Customer Service',
+          'contactOption': ['HearingImpairedSupported']
+        }
+      ]
+    }
   }
   return (
     <html lang='en' className='has-hover'>
       <Head {...props} />
       <body>
         <Skiplinks/>
-        <script type='application/ld+json' dangerouslySetInnerHTML={{ __html: JSON.stringify(schemaTags) }}/>
+        {showSchema && <script type='application/ld+json' dangerouslySetInnerHTML={{ __html: JSON.stringify(schemaTags) }}/>}
         <div id='app' className='flex-wrapper'>
           {props.children}
         </div>
