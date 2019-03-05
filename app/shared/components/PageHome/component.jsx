f045076f64cb98823e8dbf08c4db1ad0bb3d4101
Joel Mitchell <joel.mitchell@cxpartners.co.uk>
2019-03-05 16:00:36 +0000

    Excess class removal

--- app/shared/components/PageHome/component.jsx
+++ app/shared/components/PageHome/component.jsx
@@ -35,6 +35,7 @@ export default class PageHome extends React.PureComponent {
       <React.Fragment>
         <Masthead path={this.props.location}/>
         <Hero {...this.props.hero}/>
+
         <Accent className='accent--muted' modifier='wrapper--constant constrain'>
           <Form role='search' className='form--search'>
             <FormGroupAutocomplete
