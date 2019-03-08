393749cf3cfa7acfa38f5ca6291d49100d7122c5
Joel Mitchell <joel.mitchell@cxpartners.co.uk>
2019-03-08 10:21:41 +0000

    Linting

--- app/shared/components/Video/component.jsx
+++ app/shared/components/Video/component.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 const Video = props => {
-	let url = props.embedUrl ? props.embedUrl + '?rel=0' : null
+  let url = props.embedUrl ? props.embedUrl + '?rel=0' : null
   return (
     <figure className={`video ${props.className}`}>
       {url && <iframe src={url} width='500' height='281' frameBorder='0' allowFullScreen='allowfullscreen'></iframe>}
