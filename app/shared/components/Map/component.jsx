7c5735501d33aa330848c165861f5531ad206ee3
Joel Mitchell <joel.mitchell@cxpartners.co.uk>
2019-03-13 16:11:47 +0000

    Catching

--- app/shared/components/Map/component.jsx
+++ app/shared/components/Map/component.jsx
@@ -2,17 +2,23 @@ import React from 'react'
 
 export default class GMap extends React.PureComponent {
   componentDidMount() {
-    const latLong = {lat: this.props.location.lat, lng: this.props.location.lon}
-    let map = new google.maps.Map(document.getElementById('map'), {
-      center: latLong,
-      zoom: 16,
-      mapTypeControl: false
-    })
-    let marker = new google.maps.Marker({
-      position: latLong,
-      map: map,
-      title: this.props.name
-    })
+    const mapCall = () => {
+      if (window.google) {
+        const latLong = {lat: this.props.location.lat, lng: this.props.location.lon}
+        let map = new google.maps.Map(document.getElementById('map'), {
+          center: latLong,
+          zoom: 16,
+          mapTypeControl: false
+        })
+        let marker = new google.maps.Marker({
+          position: latLong,
+          map: map,
+          title: this.props.name
+        })
+      }
+    }
+
+    setTimeout(mapCall, 250)
   }
 
   render() {
