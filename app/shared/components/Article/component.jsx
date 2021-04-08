2d07cca4eeb497cf87488bde310b097a5a426c9f
Ashley Brown <digitalizedelements@gmail.com>
2021-04-08 12:46:37 +0100

    moving time position

--- app/shared/components/Article/component.jsx
+++ app/shared/components/Article/component.jsx
@@ -26,9 +26,9 @@ const Article = props => {
         {props.isAmp ? props.fields.image && <AmpPicture video={video} {...props.fields.image}/> : props.fields.image && <Picture noAlt={props.fields.slug ? true : null} video={video} className={fieldIncludesVideo(props.fields.headerVideo) ? 'has-video' : ''} {...props.fields.image}/>}
         <div className={`list-item__inner${props.fields.slug == null ? ' list-item__inner--indented' : ''}`}>
           <Heading type={props.fields.type} className='list-item__title h3 heading-inline' text={`<span>${props.fields.title}</span>`}/>
-          {props.date && <Time time={props.dateFormatted} dateTime={props.date}>{(fieldIncludesVideo(video) && props.fields.image) && <Play className='hidden--sm-up icon--offset'/>}</Time>}
           {props.fields.summary && !props.fields.image && <Longform text={props.fields.summary}/>}
           {(props.fields.slug && !props.fields.image) && <p className='link-text' aria-hidden='true'>{video ? 'Watch now' : 'Read more'}</p>}
+          {props.date && <Time time={props.dateFormatted} dateTime={props.date}>{(fieldIncludesVideo(video) && props.fields.image) && <Play className='hidden--sm-up icon--offset'/>}</Time>}
         </div>
       </Link>
     </Tag>
