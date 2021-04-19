492427d069932754bbe38fba864044559863e990
Ashley Brown <digitalizedelements@gmail.com>
2021-04-19 05:32:26 +0100

    add autcomplete

--- app/shared/components/PageHome/component.jsx
+++ app/shared/components/PageHome/component.jsx
@@ -15,52 +15,64 @@ import Icon from '../Icon/component.jsx'
 import WarningBar from '../AlertWarningBar/component.jsx'
 import FormGroupAutocomplete from '../FormGroupAutocomplete/component.jsx'
 import SiteMessageContainer from '../../containers/SiteMessageContainer/component.jsx'
-
+import AccessibleSearch from '../AccessibleSearch/component.jsx'
 export default class PageHome extends React.PureComponent {
-  render () {
+  render() {
     let iconSubmit = {
       label: 'Submit search',
       url: '/ui/svg/magnifying-pink.svg'
     }
+
     return (
       <React.Fragment>
-        <Masthead path={this.props.location}/>
+        <Masthead path={this.props.location} />
         <Main>
-          <Hero {...this.props.hero}/>
-          <Accent className='accent--muted' modifier='wrapper--constant'>
-            <SiteMessageContainer
-              path={this.props.location}
-              body={true}
-            />
-            <Form role='search' className='form--search constrain form--search-home'>
+          <Hero {...this.props.hero} />
+          <Accent className="accent--muted" modifier="wrapper--constant">
+            <SiteMessageContainer path={this.props.location} body={true} />
+            <Form
+              role="search"
+              className="form--search constrain form--search-home"
+            >
               <FormGroupAutocomplete
-               button='true'
-               id='homepage-large-search'
-               className='react-autosuggest__suggestions-container--homepage'
-               label='Search for any drug…'
-               placeholder='Enter a drug (e.g. Mandy)'
-               />
+                button="true"
+                id="homepage-large-search"
+                className="react-autosuggest__suggestions-container--homepage"
+                label="Search for any drug…"
+                placeholder="Enter a drug (e.g. Mandy)"
+              />
+              <AccessibleSearch />
             </Form>
           </Accent>
-          {this.props.featuredItemBlock &&
-            <section className='wrapper spacing-top--large'>
+          {this.props.featuredItemBlock && (
+            <section className="wrapper spacing-top--large">
               <Grid>
-                <GridCol className='col-12 col-sm-10 offset-sm-1 list-offset list-offset--single'>
-                  <Article {...this.props.featuredItemBlock}/>
+                <GridCol className="col-12 col-sm-10 offset-sm-1 list-offset list-offset--single">
+                  <Article {...this.props.featuredItemBlock} />
                 </GridCol>
               </Grid>
-            </section>}
-          {this.props.featuredNewsBlock && <section className='wrapper spacing--large'><CardDeck {...this.props.featuredNewsBlock} className='spacing-top--tight'/></section>}
-          {this.props.commonDrugsBlock &&
-            <Accent className='accent--muted'>
-              <CardDeck {...this.props.commonDrugsBlock} className='spacing-top--tight card-deck--responsive'/>
-            </Accent>}
+            </section>
+          )}
+          {this.props.featuredNewsBlock && (
+            <section className="wrapper spacing--large">
+              <CardDeck
+                {...this.props.featuredNewsBlock}
+                className="spacing-top--tight"
+              />
+            </section>
+          )}
+          {this.props.commonDrugsBlock && (
+            <Accent className="accent--muted">
+              <CardDeck
+                {...this.props.commonDrugsBlock}
+                className="spacing-top--tight card-deck--responsive"
+              />
+            </Accent>
+          )}
         </Main>
         <Footer />
-        <GA/>
-        <SiteMessageContainer
-          path={this.props.location}
-        />
+        <GA />
+        <SiteMessageContainer path={this.props.location} />
       </React.Fragment>
     )
   }
