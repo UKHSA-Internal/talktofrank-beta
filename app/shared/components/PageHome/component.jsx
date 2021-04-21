e87b0c228f986b5d060371d9091c3bedde9014f6
Ashley Brown <digitalizedelements@gmail.com>
2021-04-21 03:53:33 +0100

    remove unused code

--- app/shared/components/PageHome/component.jsx
+++ app/shared/components/PageHome/component.jsx
@@ -7,22 +7,12 @@ import Hero from '../Hero/component.jsx'
 import CardDeck from '../CardDeck/component.jsx'
 import Article from '../Article/component.jsx'
 import Grid from '../Grid/component.jsx'
-import Button from '../Button/component.jsx'
 import GridCol from '../GridCol/component.jsx'
 import Accent from '../Accent/component.jsx'
-
-import Icon from '../Icon/component.jsx'
-import WarningBar from '../AlertWarningBar/component.jsx'
-import FormGroupAutocomplete from '../FormGroupAutocomplete/component.jsx'
 import SiteMessageContainer from '../../containers/SiteMessageContainer/component.jsx'
 import AccessibleSearch from '../AccessibleSearch/component.jsx'
 export default class PageHome extends React.PureComponent {
   render() {
-    let iconSubmit = {
-      label: 'Submit search',
-      url: '/ui/svg/magnifying-pink.svg'
-    }
-
     return (
       <React.Fragment>
         <Masthead path={this.props.location} />
