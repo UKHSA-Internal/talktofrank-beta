48db0d0af5ba6c3e2656a9f74d549b9cf0952fa9
Ashley Brown <digitalizedelements@gmail.com>
2021-04-23 12:37:52 +0100

    reusable info panel component

---
+++ app/shared/components/InfoPanel/component.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Icon from '../Icon/component.jsx'
+import classNames from 'classnames'
+const InfoPanel = props => {
+  const classes = classNames('info-panel', props.className)
+
+  return (
+    <div className={classes}>
+      <div className="info-panel__heading">
+        <h5 className="info-panel__title">
+          {props.icon && <Icon url={props.icon} />}
+          <span>{props.title}</span>
+        </h5>
+      </div>
+      <div className="info-panel__wrapper">{props.children}</div>
+    </div>
+  )
+}
+
+export default InfoPanel
