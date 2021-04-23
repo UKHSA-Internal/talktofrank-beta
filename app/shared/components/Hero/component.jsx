3a5858ac42ba0140ed83eb4f7506444010a3682c
Ashley Brown <digitalizedelements@gmail.com>
2021-04-23 12:37:43 +0100

    hero style updates

--- app/shared/components/Hero/component.jsx
+++ app/shared/components/Hero/component.jsx
@@ -2,18 +2,19 @@ import React from 'react'
 import classNames from 'classnames'
 import SplitText from '../SplitText/component.jsx'
 import Picture from '../Picture/component.jsx'
-
+import Attribution from '../Attribution/component.jsx'
 const Hero = props => {
   let classes = classNames('hero', props.className)
 
   return (
     <section className={classes}>
-      {props.images && <Picture {...props.images}/>}
-      <div className='constrain-narrow'>
-        <div className='hero__inner hero__inner--constrained'>
+      {props.images && <Picture {...props.images} />}
+      <div className="constrain-narrow">
+        <div className="hero__inner hero__inner--constrained">
           <SplitText {...props.heading} />
         </div>
       </div>
+      <Attribution />
     </section>
   )
 }
