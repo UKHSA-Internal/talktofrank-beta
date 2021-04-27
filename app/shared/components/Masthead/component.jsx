9fa0fbcbff599abbdb1ce8ed494fb14189336278
Ashley Brown <digitalizedelements@gmail.com>
2021-04-27 09:28:08 +0100

    add in accessible search

--- app/shared/components/Masthead/component.jsx
+++ app/shared/components/Masthead/component.jsx
@@ -3,17 +3,16 @@ import classNames from 'classnames'
 import Logo from '../Logo/component.jsx'
 import Button from '../Button/component.jsx'
 import ButtonGroup from '../ButtonGroup/component.jsx'
-import FormGroupAutocomplete from '../FormGroupAutocomplete/component.jsx'
-import Form from '../Form/component.jsx'
 import Nav from '../Nav/component.jsx'
 import CookieBar from '../CookieBar/component.jsx'
 import Icon from '../Icon/component.jsx'
 import Anchor from '../Anchor/component.jsx'
 import { primary } from '../../fixtures/navigation.js'
 import { ViewportMobile, ViewportDefault } from '../Breakpoints/component.jsx'
+import AccessibleSearch from '../AccessibleSearch/component.jsx'
 
 export default class Masthead extends React.PureComponent {
-  constructor () {
+  constructor() {
     super()
     this.state = {
       mobileMenuOpen: false,
@@ -21,7 +20,7 @@ export default class Masthead extends React.PureComponent {
     }
   }
 
-  handleSearchClick () {
+  handleSearchClick() {
     const el = document.documentElement.classList
     this.setState({
       takeover: !this.state.takeover
@@ -30,13 +29,13 @@ export default class Masthead extends React.PureComponent {
     this.state.takeover ? el.remove('html-takeover') : el.add('html-takeover')
   }
 
-  handleMenuClick () {
+  handleMenuClick() {
     this.setState({
       mobileMenuOpen: !this.state.mobileMenuOpen
     })
   }
 
-  render () {
+  render() {
     // @joel @refactor - remove this into container
     let icon = {
       label: 'search',
@@ -56,54 +55,114 @@ export default class Masthead extends React.PureComponent {
     let current = this.props.path ? this.props.path.pathname : ''
 
     return (
-      <header className='masthead'>
-        <span className='visually-hidden'>Talk to Frank - Honest information about drugs</span>
+      <header className="masthead">
+        <span className="visually-hidden">
+          Talk to Frank - Honest information about drugs
+        </span>
         <CookieBar />
-        <div className='masthead__inner'>
-          <section className='navigation-wrapper'>
+        <div className="masthead__inner">
+          <section className="navigation-wrapper">
             <ViewportMobile>
-              <Button aria-hidden='false' className={this.state.mobileMenuOpen ? 'navbar-toggler active' : 'navbar-toggler'} aria-controls='navigation-primary' aria-haspopup={!this.state.mobileMenuOpen} aria-expanded={this.state.mobileMenuOpen} aria-label={this.state.mobileMenuOpen ? 'Close menu' : 'Show menu'} clickHandler={this.handleMenuClick.bind(this)}>
+              <Button
+                aria-hidden="false"
+                className={
+                  this.state.mobileMenuOpen
+                    ? 'navbar-toggler active'
+                    : 'navbar-toggler'
+                }
+                aria-controls="navigation-primary"
+                aria-haspopup={!this.state.mobileMenuOpen}
+                aria-expanded={this.state.mobileMenuOpen}
+                aria-label={
+                  this.state.mobileMenuOpen ? 'Close menu' : 'Show menu'
+                }
+                clickHandler={this.handleMenuClick.bind(this)}
+              >
                 {this.state.mobileMenuOpen ? 'Close' : 'Menu'}
               </Button>
             </ViewportMobile>
             <ViewportDefault>
-              <Button aria-hidden='true' className={this.state.mobileMenuOpen ? 'navbar-toggler active' : 'navbar-toggler'} aria-controls='navigation-primary' aria-haspopup={!this.state.mobileMenuOpen} aria-expanded={this.state.mobileMenuOpen} aria-label={this.state.mobileMenuOpen ? 'Close menu' : 'Show menu'} clickHandler={this.handleMenuClick.bind(this)}>
+              <Button
+                aria-hidden="true"
+                className={
+                  this.state.mobileMenuOpen
+                    ? 'navbar-toggler active'
+                    : 'navbar-toggler'
+                }
+                aria-controls="navigation-primary"
+                aria-haspopup={!this.state.mobileMenuOpen}
+                aria-expanded={this.state.mobileMenuOpen}
+                aria-label={
+                  this.state.mobileMenuOpen ? 'Close menu' : 'Show menu'
+                }
+                clickHandler={this.handleMenuClick.bind(this)}
+              >
                 {this.state.mobileMenuOpen ? 'Close' : 'Menu'}
               </Button>
             </ViewportDefault>
-            <Logo url='/ui/svg/logo-frank--alt.svg' alt=''/>
+            <Logo url="/ui/svg/logo-frank--alt.svg" alt="" />
             <ViewportMobile>
-              <Nav className={navClasses} menu-open={this.state.mobileMenuOpen} id='navigation-primary' navigation={primary} current={current} aria-label='Main Menu' type='nav'/>
+              <Nav
+                className={navClasses}
+                menu-open={this.state.mobileMenuOpen}
+                id="navigation-primary"
+                navigation={primary}
+                current={current}
+                aria-label="Main Menu"
+                type="nav"
+              />
             </ViewportMobile>
             <ViewportDefault>
-              <Nav className={navClasses} hasPopup={true} menu-open={true} id='navigation-primary' navigation={primary} current={current} aria-label='Main Menu' type='nav'/>
+              <Nav
+                className={navClasses}
+                hasPopup={true}
+                menu-open={true}
+                id="navigation-primary"
+                navigation={primary}
+                current={current}
+                aria-label="Main Menu"
+                type="nav"
+              />
             </ViewportDefault>
           </section>
-          <ButtonGroup className='button-group--static'>
-            <Button className='btn--flat btn--small' clickHandler={this.handleSearchClick.bind(this)}><span className='hidden--md' aria-label='Open search feature'><span aria-hidden='true'>Search</span> </span><Icon {...icon}/></Button>
-            <Anchor className='btn btn--link btn--small hidden--rg link-text' label='Call Frank on 0300 1236600' href='tel:03001236600'><span className='nav-link'>0300 1236600</span></Anchor>
+          <ButtonGroup className="button-group--static">
+            <Button
+              className="btn--flat btn--small"
+              clickHandler={this.handleSearchClick.bind(this)}
+            >
+              <span className="hidden--md" aria-label="Open search feature">
+                <span aria-hidden="true">Search</span>{' '}
+              </span>
+              <Icon {...icon} />
+            </Button>
+            <Anchor
+              className="btn btn--link btn--small hidden--rg link-text"
+              label="Call Frank on 0300 1236600"
+              href="tel:03001236600"
+            >
+              <span className="nav-link">0300 1236600</span>
+            </Anchor>
           </ButtonGroup>
         </div>
-        {this.state.takeover && <section className='masthead__takeover'>
-          <div className='masthead__takeover__inner'>
-            <Form className='form--search' role='search'>
-              <FormGroupAutocomplete
-                button='true'
-                modifiers='form-control--search'
-                className='input-group-autocomplete'
-                id='search-masthead'
-                label='Search for any drug'
-                labelHidden='true'
-                focus='true'
-                showContent={false}
-                placeholder='Enter drug name (e.g. Mandy)'
-              />
-
-            </Form>
-            <Button className='btn--flat close' clickHandler={this.handleSearchClick.bind(this)}><Icon {...iconClose}/></Button>
-          </div>
-        </section>}
-        {this.state.takeover && <div className='takeover-bg' onClick={this.handleSearchClick.bind(this)}/>}
+        {this.state.takeover && (
+          <section className="masthead__takeover">
+            <div className="masthead__takeover__inner">
+              <AccessibleSearch isSearch />
+              <Button
+                className="btn--flat close"
+                clickHandler={this.handleSearchClick.bind(this)}
+              >
+                <Icon {...iconClose} />
+              </Button>
+            </div>
+          </section>
+        )}
+        {this.state.takeover && (
+          <div
+            className="takeover-bg"
+            onClick={this.handleSearchClick.bind(this)}
+          />
+        )}
       </header>
     )
   }
