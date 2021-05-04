3eab9becd5f47147600075cbf294231fdbcfb209
Ashley Brown <digitalizedelements@gmail.com>
2021-05-04 15:24:20 +0100

    info panel refactor

--- app/shared/components/QuickInfoPanel/component.jsx
+++ app/shared/components/QuickInfoPanel/component.jsx
@@ -11,6 +11,10 @@ class QuickInfoPanel extends React.Component {
       if (!btn.classList.contains('collapsible__trigger--active')) {
         btn.click()
       }
+      console.log(
+        'scrolling to:',
+        document.querySelector(`#section-${section.replace('#', '')}`)
+      )
       scrollIntoView(
         document.querySelector(`#section-${section.replace('#', '')}`)
       )
@@ -21,75 +25,93 @@ class QuickInfoPanel extends React.Component {
       <div className="quick-info-panel">
         <div className="row">
           <div className="quick-info-panel__heading">
-            <h5 className="quick-info-panel__title">Quick info</h5>
+            <h5 className="quick-info-panel__heading_title">Quick info</h5>
+          </div>
+          <div className="quick-info-panel__wrapper col-12">
+            <p className="quick-info-panel__warning">
+              How you experience the drug varies from person to person
+            </p>
           </div>
           {this.props.quickInfoPanelFeelings && (
             <div className="quick-info-panel__wrapper col-12">
-              <a
-                onClick={e => this.handleClick(e, '#how-it-feels')}
-                className="quick-info-panel__link"
-                href="#how-it-feels"
-              >
-                How you might feel
-              </a>
+              <p className="quick-info-panel_title">How you might feel</p>
+
               <p className="quick-info-panel__description">
-                {this.props.quickInfoPanelFeelings}
+                {this.props.quickInfoPanelFeelings}{' '}
+                <a
+                  onClick={e => this.handleClick(e, '#how-it-feels')}
+                  className="quick-info-panel__link"
+                  href="#how-it-feels"
+                >
+                  (Read more)
+                </a>
               </p>
             </div>
           )}
           {this.props.quickInfoPanelEffects && (
             <div className="quick-info-panel__wrapper col-12">
-              <a
-                onClick={e => this.handleClick(e, '#how-it-feels')}
-                className="quick-info-panel__link"
-                href="#how-it-feels"
-              >
-                Effects on your body
-              </a>
+              <p className="quick-info-panel_title">Effects on your body</p>
+
               <p className="quick-info-panel__description">
-                {this.props.quickInfoPanelEffects}
+                {this.props.quickInfoPanelEffects}{' '}
+                <a
+                  onClick={e => this.handleClick(e, '#how-it-feels')}
+                  className="quick-info-panel__link"
+                  href="#how-it-feels"
+                >
+                  (Read more)
+                </a>
               </p>
             </div>
           )}
-          {this.props.quickInfoPanelTimeToKickIn && (
-            <div className="quick-info-panel__wrapper col-6">
-              <a
-                onClick={e => this.handleClick(e, '#how-it-feels')}
-                className="quick-info-panel__link"
-                href="#how-it-feels"
-              >
-                Time to kick in
-              </a>
+          {this.props.quickInfoPanelTimeToKickIn &&
+            this.props.quickInfoPanelDuration && (
+              <div className="quick-info-panel__wrapper col-12">
+                <p className="quick-info-panel_title">Duration</p>
+
+                <p className="quick-info-panel__description">
+                  <p>
+                    Average time to kick in:{' '}
+                    {this.props.quickInfoPanelTimeToKickIn}{' '}
+                  </p>
+                  <p>Average duration: {this.props.quickInfoPanelDuration} </p>
+                  <a
+                    onClick={e => this.handleClick(e, '#how-it-feels')}
+                    className="quick-info-panel__link"
+                    href="#how-it-feels"
+                  >
+                    (Read more)
+                  </a>
+                </p>
+              </div>
+            )}
+          {this.props.quickInfoPanelRisks && (
+            <div className="quick-info-panel__wrapper  col-12">
+              <p className="quick-info-panel_title">Risks</p>
               <p className="quick-info-panel__description">
-                {this.props.quickInfoPanelTimeToKickIn}
-              </p>
-            </div>
-          )}
-          {this.props.quickInfoPanelDuration && (
-            <div className="quick-info-panel__wrapper col-6">
-              <a
-                onClick={e => this.handleClick(e, '#duration')}
-                className="quick-info-panel__link"
-                href="#duration"
-              >
-                How long it lasts
-              </a>
-              <p className="quick-info-panel__description">
-                {this.props.quickInfoPanelDuration}
+                {this.props.quickInfoPanelRisks}{' '}
+                <a
+                  onClick={e => this.handleClick(e, '#the-risks')}
+                  className="quick-info-panel__link"
+                  href="#the-risks"
+                >
+                  (Read more)
+                </a>
               </p>
             </div>
           )}
           {this.props.quickInfoPanelMixing && (
             <div className="quick-info-panel__wrapper  col-12">
-              <a
-                onClick={e => this.handleClick(e, '#mixing')}
-                className="quick-info-panel__link"
-                href="#mixing"
-              >
-                Mixing with other drugs
-              </a>
+              <p className="quick-info-panel_title">Dangerous mixes</p>
               <p className="quick-info-panel__description">
-                {this.props.quickInfoPanelMixing}
+                {this.props.quickInfoPanelMixing}{' '}
+                <a
+                  onClick={e => this.handleClick(e, '#mixing')}
+                  className="quick-info-panel__link"
+                  href="#mixing"
+                >
+                  Mixing with other drugs
+                </a>
               </p>
             </div>
           )}
