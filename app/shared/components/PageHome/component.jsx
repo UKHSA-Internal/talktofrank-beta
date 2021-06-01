2bb2aacb06080e47f8dbbf4b386c201a6e4e6de8
Dave Martin <1011822+davelovemartin@users.noreply.github.com>
2021-06-01 14:20:33 +0100

    Fix/scrolling issues (#313)
    * improving homepage scrolling
    
    * fix linting errors
    
    * ensuring that toggles behaviour is consistent
    
    * moving scroll into componentDidUpdate after state change

--- app/shared/components/PageHome/component.jsx
+++ app/shared/components/PageHome/component.jsx
@@ -17,7 +17,7 @@ import BlockFrankAdvice from '../BlockFrankAdvice/component.jsx'
 import PickADrug from '../PickADrug/component.jsx'
 import BlockFeaturedVideo from '../BlockFeaturedVideo/component.jsx'
 import AttributedImage from '../AttributedImage/component.jsx'
-import { isInBrowser } from '../../utilities'
+import { isInBrowser, scrollIntoViewFromCurrent } from '../../utilities'
 import HelpPanels from '../HelpPanels/component.jsx'
 
 export default class PageHome extends React.Component {
@@ -78,9 +78,7 @@ export default class PageHome extends React.Component {
         this[`drugsgrid__panel-${this.state.selected}`] &&
         this[`drugsgrid__panel-${this.state.selected}`].current
       ) {
-        this[`drugsgrid__panel-${this.state.selected}`].current.scrollIntoView({
-          behavior: 'smooth'
-        })
+        scrollIntoViewFromCurrent(this[`drugsgrid__panel-${this.state.selected}`].current)
       }
     }
   }
@@ -134,6 +132,7 @@ export default class PageHome extends React.Component {
                         aria-labelledby={`druggrid__button-${drug.slug}`}
                         role="region"
                         hidden={drug.slug !== this.state.selected}
+                        ref={this[`drugsgrid__panel-${drug.slug}`]}
                       >
                         <div className="flex justify-content-between align-items-start">
                           <div className="m-b-60">
